dbd4f35e11ef1c589fdd3e244dda0dcf7c356dfb
5ia17sihuster <5ia17sihuster@gibwintra.ch>
2019-01-10 08:35:13 +0100

    sign_up.php page

---
+++ sign_up.php
@@ -0,0 +1,66 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Registration</h1>
+
+    <form action="">
+        <div>
+            <label>Firstname</label>
+            <input type="text" name="firstname">
+        </div>
+        <div>
+            <label>Lastname</label>
+            <input type="text" name="lastname">
+        </div>
+        <div>
+            <label>age</label>
+            <input type="number" name="age">
+        </div>
+        <div>
+            <label>gender</label>
+            <input type="text" name="gender">
+        </div>
+        <div>
+            <label>street</label>
+            <input type="text" name="street">
+        </div>
+        <div>
+            <label>location</label>
+            <input type="text" name="location">
+        </div>
+        <div>
+            <label>zip</label>
+            <input type="number" name="zip">
+        </div>
+        <div>
+            <label>country</label>
+            <input type="text" name="country">
+        </div>
+        <div>
+            <label>email</label>
+            <input type="email" name="email">
+        </div>
+        <div>
+            <label>year</label>
+            <input type="text" name="year">
+        </div>
+        <hr />
+        <div>
+            <label>password</label>
+            <input type="text" name="password">
+        </div>
+        <div>
+            <label>Confirm password</label>
+            <input type="text" name="confirmPassword">
+        </div>
+    </form>
+</body>
+</html>
