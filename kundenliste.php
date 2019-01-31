473448654800c8194fe80d6fd80ccec2d749785c
5ia17jogautschi <5ia17jogautschi@gibwintra.ch>
2019-01-31 08:18:02 +0100

    Kundenliste nav

--- kundenliste.php
+++ kundenliste.php
@@ -29,7 +29,84 @@
 
     //table
     
-?>
+    if ($_GET["kid"] != 0){
+        $kunde = $_GET["kid"];
+      
+        $sql2= "SELECT kundenid, firstname FROM kunde WHERE kundenid=" . $kunde;
+      
+        $result2 = $db->query($sql2);
+        $row2 = $result2->fetch_assoc();
+      
+       } else {
+         $kunde = 0;
+       }
+      ?>
+      
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>                        
+            </button>
+             <?php echo "<a class='navbar-brand' href='index.php?kid=" . $kunde . "'>WebSiteName</a>"; ?>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+            <li class="active"> <?php echo "<a href='index.php?kid=" . $kunde . "'>Home</a>"; ?></li>
+            <li class="active"><a href="kundenliste.php">Kundenliste</a></li>
+              <li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Kategorien<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+      
+                <?php
+                                  $sql = "SELECT id, cgname FROM kategorie";
+      
+                                  $result = $db->query($sql);
+                                  
+                                  if ($result->num_rows > 0) {
+                                      
+                                      while($row = $result->fetch_assoc()) {
+                                          echo "<li><a href='Products.php?id=" . $row["id"] . "&kid=" . $kunde . "'>". $row["cgname"]."</a></li>";
+                                      }
+                                  }
+                              ?>
+                </ul>
+                </li>
+                            <?php
+                             
+                             
+                            
+                            echo"<li><a href='Kontakt.php?kid=" . $kunde . "'>Kontakt</a></li>"; ?>
+                            
+                          </ul>
+            <ul class="nav navbar-nav navbar-right">
+              
+              <?php 
+              
+              
+      
+              if($kunde != 0){
+              
+                echo "<li><a href='kunde.php?kid=" . $kunde . "'><span></span>" . $row2["firstname"] . "</a></li>";
+      
+      
+              }else{
+      
+                echo "<li><a href='sign_up.php?kid=" .$kunde . "'><span class='glyphicon glyphicon-user'></span> Sign Up</a></li>
+                <li><a href='login.php?kid=" . $kunde . "'><span class='glyphicon glyphicon-log-in'></span> Login</a></li>";
+              }
+      
+              
+      
+              
+              
+              ?>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
     <div class="container">
         <h2>Filterable Table</h2>
